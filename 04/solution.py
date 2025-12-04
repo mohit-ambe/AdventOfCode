6b99e3dfb1ae253b59367c1b40975c9a25ee2b2c
Mohit Ambe <mambe@purdue.edu>
2025-12-04 00:14:51 -0500

    Day 4

--- 04/solution.py
+++ 04/solution.py
@@ -2,14 +2,32 @@ file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
-
-def part_one():
-    print()
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, 1), (1, -1), (-1, -1)]
 
 
-def part_two():
-    print()
+def both_parts():
+    S = set()
+    for r, line in enumerate(myin):
+        for c, char in enumerate(line):
+            if char == "@":
+                S.add((r, c))
+    original = len(S)
+
+    remove = set()
+    p1 = False
+    while remove != set() or p1 is False:
+        S = S - remove
+        remove = set()
+        for s in S:
+            y, x = s
+            count = len([s for dy, dx in directions if (y + dy, x + dx) in S])
+            if count < 4:
+                remove.add((y, x))
+        if not p1:
+            print(len(remove))
+            p1 = True
+
+    print(original - len(S))
 
 
-part_one()
-part_two()
+both_parts()
