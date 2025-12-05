414e5f3dff7953c05dadc27d8ce3970f82541ebe
Mohit Ambe <mambe@purdue.edu>
2025-12-05 00:53:57 -0500

    Day 5

--- 05/solution.py
+++ 05/solution.py
@@ -2,13 +2,43 @@ file = open("input.txt", "r")
 myin = [line.strip() for line in file.readlines()]
 file.close()
 
+ranges = [list(map(int, line.split("-"))) for line in myin[:myin.index("")]]
+nums = list(map(int, myin[myin.index("") + 1:]))
+
 
 def part_one():
-    print()
+    fresh = 0
+    for ingredient in nums:
+        for a, b in ranges:
+            if a <= ingredient <= b:
+                fresh += 1
+                break
+
+    print(fresh)
 
 
 def part_two():
-    print()
+    ranges.sort()
+
+    change = True
+    while change:
+        change = False
+        i = 0
+        while i < len(ranges):
+            a, b = ranges[i]
+            j = i + 1
+            while j < len(ranges):
+                c, d = ranges[j]
+                if a <= c <= b or a <= d <= b or c <= a <= d or c <= b <= d:
+                    a, b = min([a, b, c, d]), max([a, b, c, d])
+                    ranges[i] = [a, b]
+                    ranges.pop(j)
+                    change = True
+                    break
+                j += 1
+            i += 1
+
+    print(sum([b - a + 1 for a, b in ranges]))
 
 
 part_one()
